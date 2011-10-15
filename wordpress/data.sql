995c38318391b6ae2db22f0c55e7c12b4ed353a6
zhdanov <yuriy.zhdanov@gmail.com>
2011-10-15 20:06:01 +0400

    #3 Операции с деньгами в БД

--- wordpress/data.sql
+++ wordpress/data.sql
@@ -1,20 +1,21 @@
 CREATE TABLE IF NOT EXISTS `v2p_post_visitor` (
-  `id` int(11) NOT NULL auto_increment,
+  `post_visitor_id` int(11) NOT NULL auto_increment,
   `post` int(11) NOT NULL,
   `visitor` int(11) NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 CREATE TABLE IF NOT EXISTS `v2p_post_transaction` (
-  `id` int(11) NOT NULL auto_increment,
+  `post_transaction_id` int(11) NOT NULL auto_increment,
   `post` int(11) NOT NULL,
   `transaction` date NOT NULL,
   `cost` decimal(10,2) NOT NULL,
+  `request_transaction_id` int(11) NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 CREATE TABLE IF NOT EXISTS `v2p_request_transaction` (
-  `id` int(11) NOT NULL auto_increment,
+  `request_transaction_id` int(11) NOT NULL auto_increment,
   `user_id` int(11) NOT NULL,
   `request` date NOT NULL,
   `transaction` date NOT NULL,
