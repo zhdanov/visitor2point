76c7aaa807d92a6ca45cb886b0ba86aaad5881e8
zhdanov <yuriy.zhdanov@gmail.com>
2011-10-15 19:56:04 +0400

    #3 Профиль в wordpress и новая таблица

--- wordpress/data.sql
+++ wordpress/data.sql
@@ -1,13 +1,22 @@
-CREATE TABLE IF NOT EXISTS `vp_post2visitor` (
+CREATE TABLE IF NOT EXISTS `v2p_post_visitor` (
   `id` int(11) NOT NULL auto_increment,
   `post` int(11) NOT NULL,
   `visitor` int(11) NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
-CREATE TABLE IF NOT EXISTS `vp_post2transaction` (
+CREATE TABLE IF NOT EXISTS `v2p_post_transaction` (
   `id` int(11) NOT NULL auto_increment,
   `post` int(11) NOT NULL,
+  `transaction` date NOT NULL,
+  `cost` decimal(10,2) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `v2p_request_transaction` (
+  `id` int(11) NOT NULL auto_increment,
+  `user_id` int(11) NOT NULL,
+  `request` date NOT NULL,
   `transaction` date NOT NULL,
   `cost` decimal(10,2) NOT NULL,
   PRIMARY KEY  (`id`)
