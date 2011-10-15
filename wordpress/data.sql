abcc63a47aa0d37f1adaee0547e1d059ae56e56d
zhdanov <yuriy.zhdanov@gmail.com>
2011-10-15 20:11:14 +0400

    #3 Правка дампа для wordpress

--- wordpress/data.sql
+++ wordpress/data.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `v2p_post_visitor` (
   `post_visitor_id` int(11) NOT NULL auto_increment,
   `post` int(11) NOT NULL,
   `visitor` int(11) NOT NULL,
-  PRIMARY KEY  (`id`)
+  PRIMARY KEY  (`post_visitor_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 CREATE TABLE IF NOT EXISTS `v2p_post_transaction` (
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS `v2p_post_transaction` (
   `transaction` date NOT NULL,
   `cost` decimal(10,2) NOT NULL,
   `request_transaction_id` int(11) NOT NULL,
-  PRIMARY KEY  (`id`)
+  PRIMARY KEY  (`post_transaction_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 CREATE TABLE IF NOT EXISTS `v2p_request_transaction` (
@@ -20,5 +20,6 @@ CREATE TABLE IF NOT EXISTS `v2p_request_transaction` (
   `request` date NOT NULL,
   `transaction` date NOT NULL,
   `cost` decimal(10,2) NOT NULL,
-  PRIMARY KEY  (`id`)
+  `description` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY  (`request_transaction_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
